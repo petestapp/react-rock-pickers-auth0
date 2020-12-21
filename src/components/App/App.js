b06ba12122d6982ecc99977f7b8eecf6ff344c43
Chad Smith <chad@codingbychad.com>
2020-12-21 16:43:10 -0600

    update react version

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import RockCounter from '../RockCounter/RockCounter'
+import RockCounter from '../RockCounter/RockCounter';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>React Rock Pickers</h1>
-          <p>"You ain't ever worked a day until you worked a day picking rocks." - Mike Schlangen</p>
-        </header>
-        <h2>Luke</h2>
-        <RockCounter />
-        <h2>JJ</h2>
-        <RockCounter />
-        <h2>Sam</h2>
-        <RockCounter />
-        <h2>Pete</h2>
-        <RockCounter />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>React Rock Pickers</h1>
+        <p>
+          "You ain't ever worked a day until you worked a day picking rocks." -
+          Mike Schlangen
+        </p>
+      </header>
+      <h2>Luke</h2>
+      <RockCounter />
+      <h2>JJ</h2>
+      <RockCounter />
+      <h2>Sam</h2>
+      <RockCounter />
+      <h2>Pete</h2>
+      <RockCounter />
+    </div>
+  );
 }
 
 export default App;
--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 
-class RockCounter extends Component {
-  render() {
-    return (
+function RockCounter() {
+  return (
+    <div>
+      <div>Rocks Picked: 0</div>
       <div>
-          <div>
-              Rocks Picked: 0
-          </div>
-          <div>
-            <button>Increase</button>
-            <button>Decrease</button>
-            <button>Reset</button>
-          </div>
+        <button>Increase</button>
+        <button>Decrease</button>
+        <button>Reset</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default RockCounter;
