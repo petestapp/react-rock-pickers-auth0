8cd301889da427ce3fcdc34586275246e91df9aa
petestapp <63821853+petestapp@users.noreply.github.com>
2021-10-26 09:37:59 -0500

    increaseCount and decreaseCount working

--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -2,18 +2,23 @@ import React from 'react';
 import {useState} from "react";
 
 function RockCounter() {
+  // const[name, setName] = useState(null);
+  const[rocks, setRocks] = useState(0);
+
   const increaseCount = ()=>{
     console.log('in increaseCount');
+    setRocks(rocks + 1);
   }
   const decreaseCount = ()=>{
     console.log('in decreaseCount');
+    setRocks(rocks - 1);
   }
   const resetCount = ()=>{
     console.log('in resetCount');
   }
   return (
     <div>
-      <div>Rocks Picked: 0</div>
+      <div>Rocks Picked: {rocks}</div>
       <div>
         <button onClick={increaseCount}>Increase</button>
         <button onClick={decreaseCount}>Decrease</button>
