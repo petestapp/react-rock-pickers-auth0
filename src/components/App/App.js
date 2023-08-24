7e63b932d69173a2b7f8f12e1441410b5a701935
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-24 13:48:54 -0500

    added auth0 provider and browser router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import './App.css';
 import RockCounter from '../RockCounter/RockCounter';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 function App() {
-  return (
-    <div>
-      <header>
-        <h1>React Rock Pickers</h1>
-        <p>
-          "You ain't ever worked a day until you worked a day picking rocks." -
-          Mike Schlangen
-        </p>
-      </header>
-      <h2>Luke</h2>
-      <RockCounter />
-      <h2>JJ</h2>
-      <RockCounter />
-      <h2>Sam</h2>
-      <RockCounter />
-      <h2>Pete</h2>
-      <RockCounter />
-    </div>
-  );
+    return (
+        // <BrowserRouter>
+        <Auth0Provider domain={process.env.REACT_APP_AUTH0_DOMAIN} clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}>
+            <div>
+                <header>
+                    <h1>React Rock Pickers</h1>
+                    <p>
+                        "You ain't ever worked a day until you worked a day picking rocks." -
+                        Mike Schlangen
+                    </p>
+                </header>
+                <h2>Luke</h2>
+                <RockCounter/>
+                <h2>JJ</h2>
+                <RockCounter/>
+                <h2>Sam</h2>
+                <RockCounter/>
+                <h2>Pete</h2>
+                <RockCounter/>
+            </div>
+        </Auth0Provider>
+        // </BrowserRouter>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import {BrowserRouter} from "react-router-dom";
+import {Auth0ProviderWithHistory} from "./auth0-provider-with-history";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Auth0ProviderWithHistory>
+            <App />
+        </Auth0ProviderWithHistory>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
