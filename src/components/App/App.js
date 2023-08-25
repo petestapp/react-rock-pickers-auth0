0cb1c3236e5c451a5100edb5f06f25cb819a3337
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-25 08:58:14 -0500

    added loading page and conditional rendering for buttons, not fully consistent yet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './App.css';
 import RockCounter from '../RockCounter/RockCounter';
 import {Navbar} from "../navbar/navbar";
+import {useAuth0} from "@auth0/auth0-react";
 
 function App() {
+    const { isLoading } = useAuth0();
+
+    if (isLoading) {
+        return (
+            <div className="page-layout">
+                <PageLoader />
+            </div>
+        )
+    }
+
     return (
         <div>
             <Navbar/>
